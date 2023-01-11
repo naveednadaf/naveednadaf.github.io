be91e4bbd3d1be64c5d21b06dfd079a093de9808
naveednadaf <122467208+naveednadaf@users.noreply.github.com>
2023-01-11 15:44:18 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+const contact__btn = document.querySelector('.contact__btn');
+const popup = document.querySelector('.popup');
+const closeButton = document.getElementById('close-button');
+const textToCopy = 'nnn.ds2016@gmail.com';
+
+
+
+contact__btn.addEventListener('click', function(event) {
+  // Create a hidden textarea element
+  const textarea = document.createElement('textarea');
+  textarea.style.position = 'fixed';
+  textarea.style.top = '0';
+  textarea.style.left = '0';
+  textarea.style.width = '1px';
+  textarea.style.height = '1px';
+  textarea.style.padding = '0';
+  textarea.style.border = 'none';
+  textarea.style.outline = 'none';
+  textarea.style.boxShadow = 'none';
+  textarea.style.background = 'transparent';
+  textarea.value = textToCopy;
+  document.body.appendChild(textarea);
+
+
+  // Select the text
+  textarea.select();
+
+  // Copy the text
+  document.execCommand('copy');
+
+  // Remove the textarea element
+  document.body.removeChild(textarea);
+});
+
+contact__btn.addEventListener('click' , ()  => {
+    popup.showModal();
+});
+
+closeButton.addEventListener('click' , ()  => {
+    popup.close();
+});
+
